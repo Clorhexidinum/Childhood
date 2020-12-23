ec25dd516c4559006c7398fdddf99b473df48c93
Clorhexidinum <mkubekov@gmail.com>
2020-12-23 07:20:32 +0300

    Настройка заголовка и логотипа

--- wp-content/themes/childhood/functions.php
+++ wp-content/themes/childhood/functions.php
@@ -6,6 +6,8 @@ function childhood_scripts() {
   wp_enqueue_script('childhood_scripts', get_template_directory_uri() . '/assets/js/main.min.js', array(), null, true);
 };
 
+add_theme_support( 'custom-logo' );
+
 /* Вариант подключения разными скриптами
 add_action('wp_enqueue_scripts', 'childhood_styles');
 add_action('wp_enqueue_scripts', 'childhood_scripts');
